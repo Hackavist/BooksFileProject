35d51ec462bf28d330f6784c1f1e3abb1fdf1354
Xeina Tamer <xeinatamer@outlook.com>
2017-12-01 12:15:57 +0200

    Finished author class

---
+++ Books File Project/Admin/AddAuthor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Books_File_Project.Classes;
+
+namespace Books_File_Project.Admin
+{
+    public partial class AddAuthor : Form
+    {
+
+    }
+}
---
+++ Books File Project/Classes/Author.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_File_Project.Classes
+{
+    class Author
+    {
+        public string AuthorName { get; set; }
+        public string AuthorID { get; set; }
+        public string AuthorEmail { get; set; }
+
+        public Author()
+        {
+            AuthorName = "";
+            AuthorID = "";
+            AuthorEmail = "";
+        }
+        public Author(string id, string name, string email)
+        {
+            AuthorName = name;
+            AuthorID = id;
+            AuthorEmail = email;
+        }
+    }
+}
