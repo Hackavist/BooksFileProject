f98f2caf54566948ed456565b369968eda4af59a
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-01 11:05:57 +0200

    Done View all Authors form / added authors_txt

--- Books File Project/Program.cs
+++ Books File Project/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Books_File_Project.User;
 namespace Books_File_Project
 {
     static class Program
@@ -16,7 +16,7 @@ namespace Books_File_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new ViewAllAuthors ());
         }
     }
 }
