b989481d312a797d6fe2afd6a6560e1a56713ee0
Chantal Chawki <chanty98@hotmail.com>
2017-12-01 14:11:55 +0200

    last

--- Books File Project/Program.cs
+++ Books File Project/Program.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using Books_File_Project.Admin;
 using System;
 using System.Collections.Generic;
@@ -22,3 +23,28 @@ namespace Books_File_Project
         }
     }
 }
+=======
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Books_File_Project.User;
+using Books_File_Project.Admin;
+namespace Books_File_Project
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new AddAuthor ());
+        }
+    }
+}
+>>>>>>> fe3be17f2a0c62e1559eb401458a9965f46b85d9
