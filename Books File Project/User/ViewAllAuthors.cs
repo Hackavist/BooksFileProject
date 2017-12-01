107342d211405229c9f436f9e6b32829c22d8fa5
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-01 16:29:56 +0200

    no changes but lazm a3mel commit :D

--- Books File Project/User/ViewAllAuthors.cs
+++ Books File Project/User/ViewAllAuthors.cs
@@ -19,24 +19,24 @@ namespace Books_File_Project.User
 
         private void ViewAllAuthors_Load(object sender, EventArgs e)
         {
-            FileStream f = new FileStream(@"C:\Users\hazem\Source\Repos\BooksFileProject\Books File Project\User\Authors.txt",FileMode.Open);
+            FileStream f = new FileStream(@"C:\Users\hazem\Source\Repos\BooksFileProject\Books File Project\User\Authors.txt", FileMode.Open);
             StreamReader sr = new StreamReader(f);
-            while (sr.Peek()!=-1)
+            while (sr.Peek() != -1)
             {
-            char [] id = new char [5];
-            char [] name = new char [20];
-            char [] email = new char [25];
-                sr.Read(id,0,5);
-                sr.Read(name,0,20);
-                sr.Read(email,0,25);
-            string [] x = new string [3] ;
-            x[0]=new string (id);
-                x[1]=new string (name);
-                x[2]=new string (email);
-            dataGridView1.Rows.Add(x); 
+                char[] id = new char[5];
+                char[] name = new char[20];
+                char[] email = new char[25];
+                sr.Read(id, 0, 5);
+                sr.Read(name, 0, 20);
+                sr.Read(email, 0, 25);
+                string[] x = new string[3];
+                x[0] = new string(id);
+                x[1] = new string(name);
+                x[2] = new string(email);
+                dataGridView1.Rows.Add(x);
             }
             sr.Close();
             f.Close();
-        }
+        }   
     }
 }
