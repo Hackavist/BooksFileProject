cd4de7901c70206ce6ea6ae02c21849beb0aa755
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-01 17:13:58 +0200

    added sort option to view all authors

--- Books File Project/User/ViewAllAuthors.cs
+++ Books File Project/User/ViewAllAuthors.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using Books_File_Project.Classes;
 namespace Books_File_Project.User
 {
     public partial class ViewAllAuthors : Form
@@ -16,7 +17,20 @@ namespace Books_File_Project.User
         {
             InitializeComponent();
         }
+        List<Author> ListOfAuthors = new List<Author>();
+        private void DisplayInGridView(SortedDictionary <string , Author> ToDisplay)
+        {
+            dataGridView1.Rows.Clear();
+            foreach (var tmp in ToDisplay)
+            {
 
+                string[] x = new string[3];
+                x[0] = tmp.Value.AuthorID;
+                x[1] = (tmp.Value.AuthorName);
+                x[2] = tmp.Value.AuthorEmail;
+                dataGridView1.Rows.Add(x);
+            }
+        }
         private void ViewAllAuthors_Load(object sender, EventArgs e)
         {
             FileStream f = new FileStream("Authors.txt", FileMode.Open);
@@ -29,7 +43,6 @@ namespace Books_File_Project.User
                 char[] email = new char[25];
                 
                 string tmp = sr.ReadLine();
-                
                 tmp.CopyTo(0,id,0,5);
                 tmp.CopyTo(5, name, 0, 20);
                 tmp.CopyTo(25, email, 0, 25);
@@ -37,10 +50,42 @@ namespace Books_File_Project.User
                 x[0] = new string(id);
                 x[1] = new string(name);
                 x[2] = new string(email);
+                Author toadd = new Author(x[0],x[1],x[2]);
                 dataGridView1.Rows.Add(x);
+                ListOfAuthors.Add(toadd);
             }
             sr.Close();
             f.Close();
+        }
+
+        private void SortById_Click(object sender, EventArgs e)
+        {
+            SortedDictionary<string, Author> todisplay = new SortedDictionary<string, Author>();
+            for (int i = 0; i < ListOfAuthors.Count; i++)
+            {
+                todisplay[ListOfAuthors[i].AuthorID] = ListOfAuthors[i];
+            }
+            DisplayInGridView(todisplay);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SortedDictionary<string, Author> todisplay = new SortedDictionary<string, Author>();
+            for (int i = 0; i < ListOfAuthors.Count; i++)
+            {
+                todisplay[ListOfAuthors[i].AuthorName] = ListOfAuthors[i];
+            }
+            DisplayInGridView(todisplay);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SortedDictionary<string, Author> todisplay = new SortedDictionary<string, Author>();
+            for (int i = 0; i < ListOfAuthors.Count; i++)
+            {
+                todisplay[ListOfAuthors[i].AuthorEmail] = ListOfAuthors[i];
+            }
+            DisplayInGridView(todisplay);
         }   
     }
 }
