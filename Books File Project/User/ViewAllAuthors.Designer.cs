28b8defcec349d1e4c234ec79e23ddc54876beaf
hazemkhairy <hazemkhairy188@gmail.com>
2017-12-01 11:07:29 +0200

    added refrences

---
+++ Books File Project/User/ViewAllAuthors.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Books_File_Project.User
+{
+    partial class ViewAllAuthors
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Author_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Author_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Author_Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Author_ID,
+            this.Author_Name,
+            this.Author_Email});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(932, 327);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Author_ID
+            // 
+            this.Author_ID.HeaderText = "Author ID";
+            this.Author_ID.Name = "Author_ID";
+            this.Author_ID.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            // 
+            // Author_Name
+            // 
+            this.Author_Name.HeaderText = "Author Name";
+            this.Author_Name.Name = "Author_Name";
+            // 
+            // Author_Email
+            // 
+            this.Author_Email.HeaderText = "Author Email";
+            this.Author_Email.Name = "Author_Email";
+            // 
+            // ViewAllAuthors
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(956, 491);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ViewAllAuthors";
+            this.Text = "ViewAllAuthors";
+            this.Load += new System.EventHandler(this.ViewAllAuthors_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Author_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Author_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Author_Email;
+    }
+}
