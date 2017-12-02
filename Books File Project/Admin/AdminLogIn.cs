9d504a027741cb1b5659979694cd97fc9e32755d
Chantal Chawki <chanty98@hotmail.com>
2017-12-02 22:45:59 +0200

    Admin Log in DONE

--- Books File Project/Admin/AdminLogIn.cs
+++ Books File Project/Admin/AdminLogIn.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Books_File_Project.Admin;
 
 namespace Books_File_Project
 {
@@ -15,6 +17,62 @@ namespace Books_File_Project
         public AdminLogIn()
         {
             InitializeComponent();
+        }
+
+        private void LogInButton_Click(object sender, EventArgs e)
+        {
+            
+
+            string username = UserName.Text;
+            string password = Password.Text;
+
+            string[] record, field;
+
+            bool find = false;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Please Enter Username And Password");
+
+            }
+            else
+            {
+                FileStream fs = new FileStream("Admin.txt", FileMode.Open);
+                StreamReader sr = new StreamReader(fs);
+                while (sr.Peek() != -1 && find == false)
+                {
+                    record = sr.ReadLine().Split('#');
+
+                    for (int i = 0; i < record.Length-1; i++)
+                    {
+                        field = record[i].Split('@');
+                        string un = field[0];
+                        string pass = field[1];
+
+                        if (username == un && password == pass)
+                        {
+                            find = true;
+                            MessageBox.Show("Successful Log In");
+                            AdminControls ad = new AdminControls();
+                            ad.Show();
+                            this.Close();
+
+                        }
+                        else if(username!=un || password!=pass)
+                        {
+                            MessageBox.Show("Wrong Username Or Password");
+                        }
+                    }
+                    if (find == false)
+                {
+                    MessageBox.Show("Admin Not Found");
+                }
+
+                }
+               
+                sr.Close();
+                fs.Close();
+            }
         }
     }
 }
--- Books File Project/Program.cs
+++ Books File Project/Program.cs
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
-﻿using Books_File_Project.Admin;
-using System;
+
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Books_File_Project.User;
+using Books_File_Project.Admin;
 
 namespace Books_File_Project
 {
@@ -15,36 +15,15 @@ namespace Books_File_Project
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void 
+            
+            
+            Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new ViewBooksInSpecificYear());
+            Application.Run(new AdminLogIn ());
         }
     }
 }
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Books_File_Project.User;
-using Books_File_Project.Admin;
-namespace Books_File_Project
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new AddAuthor ());
-        }
-    }
-}
->>>>>>> fe3be17f2a0c62e1559eb401458a9965f46b85d9
+
