9b6917dc5919b8a2827715485bb4652abb6485cc
Nour ElGafy <nour-elgafy@outlook.com>
2017-12-06 01:28:12 +0200

    fixed the add book form bug

--- Books File Project/Admin/AddBook.cs
+++ Books File Project/Admin/AddBook.cs
@@ -23,6 +23,7 @@ namespace Books_File_Project.Admin
             }
             else
             {
+                MessageBox.Show("The Book is being Added ");
                 //writes the book using '@' dilimter only if the auther id is valid 
                 sw.Write(SerialNumber.Text);
                 sw.Write('@');
@@ -37,10 +38,10 @@ namespace Books_File_Project.Admin
                 MessageBox.Show("The Books has been Added Sucssefully ");
 
                 //resets the text boxes 
-                SerialNumber.Text = "";
-                BookName.Text     = "";
-                PublishYear.Text  = "";
-                AuthorId.Text     = "";
+                SerialNumber.Clear();
+                BookName.Clear();
+                PublishYear.Clear();
+                AuthorId.Clear();
             }
 
             sw.Close();
