8e9642c47b7756da1604c4f834dcb3b689a911a1
hadilelrody <Elrody52@gmail.com>
2017-12-07 13:01:40 -0800

    fixed the view all authers bug

--- Books File Project/User/UserWelcome.cs
+++ Books File Project/User/UserWelcome.cs
@@ -69,11 +69,6 @@ namespace Books_File_Project.User
 
         }
 
-        private void UserWelcome_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void shakes_MouseHover(object sender, EventArgs e)
         {
             shakes.Visible = false;
@@ -96,6 +91,15 @@ namespace Books_File_Project.User
         private void books_MouseLeave_1(object sender, EventArgs e)
         {
             books.Image = Resources.books;
+        }
+
+        private void viewallauthors_Click(object sender, EventArgs e)
+        {
+            ViewAllAuthors va = new ViewAllAuthors();
+            va.Show();
+            this.Hide();
+            this.Close();
+  
         }
     }
 }
