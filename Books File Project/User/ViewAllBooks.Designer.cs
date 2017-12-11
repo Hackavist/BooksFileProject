e61d08640526f4dea56bc0616edc19fb2be9a6d6
hadilelrody <Elrody52@gmail.com>
2017-12-10 21:21:45 -0800

    add panel in view all books

--- Books File Project/User/ViewAllBooks.Designer.cs
+++ Books File Project/User/ViewAllBooks.Designer.cs
@@ -39,7 +39,9 @@
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -48,10 +50,11 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(24, 201);
+            this.label1.Location = new System.Drawing.Point(36, 137);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 32);
+            this.label1.Size = new System.Drawing.Size(20, 22);
             this.label1.TabIndex = 3;
+            this.label1.Text = "1";
             // 
             // label2
             // 
@@ -59,10 +62,11 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label2.Location = new System.Drawing.Point(271, 201);
+            this.label2.Location = new System.Drawing.Point(285, 137);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 32);
+            this.label2.Size = new System.Drawing.Size(20, 22);
             this.label2.TabIndex = 4;
+            this.label2.Text = "2";
             // 
             // label3
             // 
@@ -70,10 +74,11 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label3.Location = new System.Drawing.Point(675, 201);
+            this.label3.Location = new System.Drawing.Point(628, 137);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 32);
+            this.label3.Size = new System.Drawing.Size(20, 22);
             this.label3.TabIndex = 5;
+            this.label3.Text = "3";
             // 
             // label4
             // 
@@ -81,20 +86,21 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label4.Location = new System.Drawing.Point(884, 201);
+            this.label4.Location = new System.Drawing.Point(893, 137);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 32);
+            this.label4.Size = new System.Drawing.Size(20, 22);
             this.label4.TabIndex = 6;
+            this.label4.Text = "4";
             // 
             // label5
             // 
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 26F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label5.Location = new System.Drawing.Point(439, 30);
+            this.label5.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.label5.Location = new System.Drawing.Point(424, 26);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(236, 60);
+            this.label5.Size = new System.Drawing.Size(162, 40);
             this.label5.TabIndex = 7;
             this.label5.Text = "All Books";
             this.label5.FontChanged += new System.EventHandler(this.ViewAllBooks_Load);
@@ -105,9 +111,9 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label6.Location = new System.Drawing.Point(23, 136);
+            this.label6.Location = new System.Drawing.Point(35, 96);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(241, 41);
+            this.label6.Size = new System.Drawing.Size(163, 26);
             this.label6.TabIndex = 8;
             this.label6.Text = "Serial Number";
             // 
@@ -117,9 +123,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label7.Location = new System.Drawing.Point(270, 136);
+            this.label7.Location = new System.Drawing.Point(284, 96);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(196, 41);
+            this.label7.Size = new System.Drawing.Size(130, 26);
             this.label7.TabIndex = 9;
             this.label7.Text = "Book Name";
             // 
@@ -129,9 +135,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label8.Location = new System.Drawing.Point(666, 136);
+            this.label8.Location = new System.Drawing.Point(627, 96);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(211, 41);
+            this.label8.Size = new System.Drawing.Size(142, 26);
             this.label8.TabIndex = 10;
             this.label8.Text = "Publish Year";
             // 
@@ -141,16 +147,16 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label9.Location = new System.Drawing.Point(883, 136);
+            this.label9.Location = new System.Drawing.Point(892, 96);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(175, 41);
+            this.label9.Size = new System.Drawing.Size(118, 26);
             this.label9.TabIndex = 11;
             this.label9.Text = "Author ID";
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(30, 30);
+            this.pictureBox2.Location = new System.Drawing.Point(12, 26);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(50, 50);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -158,27 +164,44 @@
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
+            // panel1
+            // 
+            this.panel1.AllowDrop = true;
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1070, 616);
+            this.panel1.TabIndex = 14;
+            // 
             // ViewAllBooks
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1070, 616);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.label9);
-            this.Controls.Add(this.label8);
-            this.Controls.Add(this.label7);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Goudy Old Style", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "ViewAllBooks";
             this.Text = "ViewAllBooks";
             this.Load += new System.EventHandler(this.ViewAllBooks_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -195,5 +218,6 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
